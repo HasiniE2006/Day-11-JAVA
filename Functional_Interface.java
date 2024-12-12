c0c4bd4f9c4137df8c7250aa5286b98c16b6c931
Hasini E <hasini1530@gmail.com>
2024-12-12 12:19:27 +0530

    Add files via upload

---
+++ Functional_Interface.java
@@ -0,0 +1,13 @@
+interface math
+{
+    public int operation(int a,int b);
+}
+class Functional_Interface                                                                                                                                                                                                          
+{
+    public static void main(String[] args) {
+        math add=(a,b)->a+b;
+        math sub=(a,b)->a-b;
+System.out.println(add.operation(4,5));
+System.out.println(sub.operation(5,6));
+    }
+}
