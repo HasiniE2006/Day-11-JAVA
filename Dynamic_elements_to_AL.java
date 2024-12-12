79f6f65feda2e128491f8ffa123068ebf6751cb3
Hasini E <hasini1530@gmail.com>
2024-12-12 15:02:15 +0530

    Add files via upload

---
+++ Dynamic_elements_to_AL.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Dynamic_elements_to_AL {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        //Accepting array
+        int n=sc.nextInt();
+        ArrayList <Integer> a=new ArrayList<>();
+        for(int i=0;i<n;i++)
+        {
+            int s=sc.nextInt();
+            a.add(s);
+        }
+        a.add(89);
+        System.out.println(a);
+    }
+}
