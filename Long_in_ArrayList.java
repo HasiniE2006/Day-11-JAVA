a168393d99c7dbe6e53a64041f413478bdf44370
Hasini E <hasini1530@gmail.com>
2024-12-12 20:43:13 +0530

    Add files via upload

---
+++ Long_in_ArrayList.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Long_in_ArrayList {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        long a=sc.nextLong();
+        long s=sc.nextInt();
+        System.out.println(a);
+        System.out.println(s);
+    }
+}
